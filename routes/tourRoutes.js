808a09c16da2727bc6956bbfa9f1f9a0a9f57b59
UzomaAkpala <francisakpala@yahoo.com>
2023-11-27 17:22:43 +0100

    Params Middleware

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,6 +4,11 @@ const { getAllTours, createTour, getTour, updateTour, deleteTour } =
   tourController;
 const router = express.Router();
 
+router.param('id', (req, res, next, val) => {
+  console.log(`Tour is is ${val}`);
+  next();
+});
+
 router.route('/').get(getAllTours).post(createTour);
 
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
