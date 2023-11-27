118709f8624967b4073940c36d61280f3b383f1a
UzomaAkpala <francisakpala@yahoo.com>
2023-11-27 14:31:08 +0100

    Handling Delete

--- app.js
+++ app.js
@@ -16,6 +16,22 @@ app.get('/api/v1/tours', (req, res) => {
     .json({ status: 'success', results: tours.length, data: { tours: tours } });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+
+  const id = Number(req.params.id);
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({ status: 'Fail', message: 'Invalid Id' });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: { tour },
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   //console.log(req.body);
 
@@ -47,19 +63,16 @@ app.patch('/api/v1/tours/:id', (req, res) => {
   });
 });
 
-app.get('/api/v1/tours/:id', (req, res) => {
-  console.log(req.params);
-
-  const id = Number(req.params.id);
-  const tour = tours.find((el) => el.id === id);
-
-  if (!tour) {
-    return res.status(404).json({ status: 'Fail', message: 'Invalid Id' });
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'Fail',
+      message: 'Invalid Id',
+    });
   }
-
-  res.status(200).json({
-    status: 'success',
-    data: { tour },
+  res.status(204).json({
+    status: 'Success',
+    data: null,
   });
 });
 
