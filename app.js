5e9befb65f39cff47dc13ca905183f77cb19b25b
UzomaAkpala <francisakpala@yahoo.com>
2023-11-27 14:26:45 +0100

    Handling Patch Request

--- app.js
+++ app.js
@@ -32,6 +32,21 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'Fail',
+      message: 'Invalid Id',
+    });
+  }
+  res.status(200).json({
+    status: 'Success',
+    data: {
+      tour: '<Update tour here...>',
+    },
+  });
+});
+
 app.get('/api/v1/tours/:id', (req, res) => {
   console.log(req.params);
 
