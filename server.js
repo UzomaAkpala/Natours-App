ed86a9d8fb6a2edb981a5d01925861d0f211ebce
UzomaAkpala <francisakpala@yahoo.com>
2023-11-27 18:52:07 +0100

    Environment Variables

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
+console.log(process.env);
+
 //SERVER
-const port = 3000;
+const port = process.env.port || 3000;
 app.listen(port, () => {
   console.log(`App listening on port ${port}...`);
 });
