92c58812e63a9fe5ab628883a5e3b2d0a1da1ae7
UzomaAkpala <francisakpala@yahoo.com>
2023-11-27 11:58:06 +0100

    Handling Post requests

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 const port = 3000;
 
@@ -13,6 +14,22 @@ app.get('/api/v1/tours', (req, res) => {
   res
     .status(200)
     .json({ status: 'success', results: tours.length, data: { tours: tours } });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  //console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({ status: 'Success', data: { tour: newTour } });
+    }
+  );
 });
 
 app.listen(3000, () => {
