6e8666c14780ec8cd42687c5399fad8371d8f9f7
UzomaAkpala <francisakpala@yahoo.com>
2023-11-28 16:59:37 +0100

    Another way of creating documents

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
-const { getAllTours, createTour, getTour, updateTour, deleteTour, checkBody } =
+const { getAllTours, createTour, getTour, updateTour, deleteTour } =
   tourController;
 const router = express.Router();
 
 //router.param('id', tourController.checkId);
 
-router.route('/').get(getAllTours).post(checkBody, createTour);
+router.route('/').get(getAllTours).post(createTour);
 
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const dotenv = require('dotenv');
 const app = require('./app');
 dotenv.config({ path: './config.env' });
 
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
-);
+// const DB = process.env.DATABASE.replace(
+//   '<PASSWORD>',
+//   process.env.DATABASE_PASSWORD
+// );
+const DB = process.env.DATABASE;
 
 mongoose
   .connect(DB, {
