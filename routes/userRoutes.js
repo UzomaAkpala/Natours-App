2ff02fa6fdb0f86ccd5ae62219d23ca2de87aef7
UzomaAkpala <francisakpala@yahoo.com>
2023-12-06 19:49:23 +0100

    Logging in user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,6 +8,7 @@ const { getAllUsers, createUser, getUser, updateUser, deleteUser } =
 const router = express.Router();
 
 router.post('/signup', authController.signup);
+router.post('/login', authController.login);
 
 router.route('/').get(getAllUsers).post(createUser);
 
